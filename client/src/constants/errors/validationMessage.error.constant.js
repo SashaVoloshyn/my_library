046cf2339346c65781aca87b76a6cb67bf87fd3a
SashaVoloshyn <sasha.voloshyn359@gmail.com>
2022-07-31 00:14:23 +0300

    test client

--- client/src/constants/errors/validationMessage.error.constant.js
+++ client/src/constants/errors/validationMessage.error.constant.js
@@ -1,14 +1,14 @@
 import {keyValidationError} from "./keyValidationError.constant";
 
 export const validationMessageErrorConstant = {
-    [keyValidationError.STRING_BASE]: '{{#label}} повинен вмістити букви',
-    [keyValidationError.STRING_EMPTY]: '{{#label}} не може бути пустим',
-    [keyValidationError.STRING_MAX]: '{{#label}} повинен мати довжину не більше ніж {#limit}',
-    [keyValidationError.STRING_MIN]: '{{#label}} повинен мати довжину не менше {#limit}',
-    [keyValidationError.STRING_PATTERN]: '{{#label}} повинен дотримуватись обов`язково регулярному виразу: {{#regex}}',
-    [keyValidationError.NUMBER_MAX]: '{{#label}} максимальне число {#limit}',
-    [keyValidationError.NUMBER_MIN]: '{{#label}} мінімальне число {#limit}',
-    [keyValidationError.NUMBER_BASE]: '{{#label}} має бути числом',
-    [keyValidationError.NUMBER_EMPTY]: '{{#label}} не може бути пустим',
-    [keyValidationError.ANY_REQUIRED]: "{{#label}} є обов'язковим",
+    [keyValidationError.STRING_BASE]: 'Поле повинно вмістити букви',
+    [keyValidationError.STRING_EMPTY]: 'поле не може бути пустим',
+    [keyValidationError.STRING_MAX]: 'довжина повинна бути  не більше ніж {#limit}',
+    [keyValidationError.STRING_MIN]: 'довжина повинна бути не меньше ніж {#limit}',
+    [keyValidationError.STRING_PATTERN]: 'поле повинно містити Великі та малі літери',
+    [keyValidationError.NUMBER_MAX]: 'максимальне число {#limit}',
+    [keyValidationError.NUMBER_MIN]: 'мінімальне число {#limit}',
+    [keyValidationError.NUMBER_BASE]: 'має бути числом',
+    [keyValidationError.NUMBER_EMPTY]: 'поле  не може бути пустим',
+    [keyValidationError.ANY_REQUIRED]: "поле є обов'язковим",
 };
